ef1194410a92f9333d57b5b1f5fcaaa2c7fac8c1
Sam Odom <samodom@me.com>
2014-11-08 14:14:57 -0600

    Initial Commit

---
+++ CoreGraphicsSwagger/CoreGraphicsSwagger.h
@@ -0,0 +1,19 @@
+//
+//  CoreGraphicsSwagger.h
+//  CoreGraphicsSwagger
+//
+//  Created by Sam Odom on 11/8/14.
+//  Copyright (c) 2014 Swagger Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreGraphicsSwagger.
+FOUNDATION_EXPORT double CoreGraphicsSwaggerVersionNumber;
+
+//! Project version string for CoreGraphicsSwagger.
+FOUNDATION_EXPORT const unsigned char CoreGraphicsSwaggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreGraphicsSwagger/PublicHeader.h>
+
+
